5119bb3f339b726b6b84f9f97827174436bed1c5
M-Aloysie <a.murekatet@alustudent.com>
2024-07-03 10:03:53 -0400

    New Commit

--- Software Project company/tasks.sql
+++ Software Project company/tasks.sql
@@ -1,4 +1,3 @@
--- tasks.sql
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: software_company
--- Software Project company/teams.sql
+++ Software Project company/teams.sql
@@ -1,4 +1,3 @@
--- teams.sql
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: software_company
